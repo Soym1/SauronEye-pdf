186f8fc51556bf64d7734e80686b62c4a2b47f6a
MrSoym <Mrsoym123@gmail.com>
2022-03-04 02:07:46 +0300

    first commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -207,7 +207,6 @@ namespace SauronEYE
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SauronEYE";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
